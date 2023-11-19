fa9d95bca70bdd7588b02acc6936e8707b971e52
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-11-19 11:58:14 -0300

    chore: add and configure jest

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/infra/repositories/postgres/helpers/connection.ts'
+  ],
+  coverageDirectory: "coverage",
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.spec.ts'],
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  },
+  setupFiles: ['dotenv/config'],
+  setupFilesAfterEnv: ['<rootDir>/tests/infra/repositories/postgres/mocks/connection.ts']
+};
+
+module.exports = config;
