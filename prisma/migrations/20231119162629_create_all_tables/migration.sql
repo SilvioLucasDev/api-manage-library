d330781e8e944a247018b8bdd5b8fd775953d96a
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-11-19 13:27:56 -0300

    feat: add tables

---
+++ prisma/migrations/20231119162629_create_all_tables/migration.sql
@@ -0,0 +1,91 @@
+-- CreateTable
+CREATE TABLE "libraries" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "libraries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "authors" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "nationality" TEXT NOT NULL,
+    "library_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "authors_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "year_publication" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "available_quantity" INTEGER NOT NULL,
+    "library_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "books_users" (
+    "id" TEXT NOT NULL,
+    "book_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "return_date" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "books_users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "library_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "libraries_id_key" ON "libraries"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "authors_id_key" ON "authors"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_id_key" ON "books"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "books_users_id_key" ON "books_users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- AddForeignKey
+ALTER TABLE "authors" ADD CONSTRAINT "authors_library_id_fkey" FOREIGN KEY ("library_id") REFERENCES "libraries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "books" ADD CONSTRAINT "books_library_id_fkey" FOREIGN KEY ("library_id") REFERENCES "libraries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "books_users" ADD CONSTRAINT "books_users_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "books_users" ADD CONSTRAINT "books_users_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_library_id_fkey" FOREIGN KEY ("library_id") REFERENCES "libraries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
